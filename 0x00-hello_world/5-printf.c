610ffe7ecdeba106c2b3188817ca68cf27c8c69d
levayathan <reapergames0159@gmail.com>
2023-06-15 17:48:46 -0700

    adding multiple scripts

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/** This code prints : "with proper grammar, but the outcome is a piece of
+ * art" */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
