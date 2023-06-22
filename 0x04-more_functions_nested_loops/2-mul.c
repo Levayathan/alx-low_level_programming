1221578d3f7eaeb4229b633ffb52297748282549
levayathan <reapergames0159@gmail.com>
2023-06-22 16:13:17 -0700

    Adding a multiplicator

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integer
+ *
+ * @a : is first integer
+ *
+ * @b : is second integer
+ *
+ * Return: result (multiplication of two integers)
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
