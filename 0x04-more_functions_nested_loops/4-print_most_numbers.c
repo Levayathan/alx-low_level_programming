21d48be888c6c11fcb09ceca7df6b4ce9f8cdb58
levayathan <reapergames0159@gmail.com>
2023-06-22 17:06:01 -0700

    print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,24 +1,19 @@
 #include "main.h"
 
 /**
- * print_most_numbers - Print numbers from 0 to 9 except 2 and 4
+ * print_most_numbers - prints numbers 0-9 except 2,4
  *
  * Return: void
  */
 
-void print_most_numbers(void);
+void print_most_numbers(void)
 {
-    int c;
+	int c;
 
-    for (c = '0'; c <= '9'; c++)
-    {
-        if (c != '2' && c != '4')
-        {
-            _putchar(c);
-        }
-    }
-
-    _putchar('\n');
-
-    return 0;
+	for (c = '0'; c <= '9'; c++)
+	{
+		if (c != '2' && c != '4')
+			_putchar(c);
+	}
+	_putchar('\n');
 }
