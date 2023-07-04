02af87bbb078a0477caa1a0e856b50899e1b351b
levayathan <reapergames0159@gmail.com>
2023-07-03 17:06:18 -0700

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,29 +1,38 @@
 #include "main.h"
 
 /**
- *_strstr - Write a function that locates a substring
- *
- * @haystack: This is the input string
- * @needle: This is a find to locate in the string
- *
- * Return: a pointer to the beginning of the located substring, or NULL if the
- * substring is not found.
+ * _strstr - locates a substring
+ * @haystack: string to search through
+ * @needle: string to match
+ * Return: pointer to initial segment of haystack
  */
 
 char *_strstr(char *haystack, char *needle)
 {
-	while (*haystack)
-	{
-		char *temp;
+	int i, j, start;
+	int nlen = 0;
 
-		temp = haystack;
-		while (*needle == *haystack)
+	i = 0;
+	j = 0;
+	start = 0;
+
+	while (needle[nlen] != '\0')
+	{
+		nlen++;
+	}
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; j < nlen && haystack[i] == needle[j]; j++, i++)
 		{
-			haystack++, needle++;
+			if (j == 0)
+			{
+				start = i;
+			}
+			if (j == nlen - 1)
+			{
+				return (haystack + start);
+			}
 		}
-		if (*needle == '\0')
-			return (temp);
-		haystack++;
 	}
 	return (0);
 }
