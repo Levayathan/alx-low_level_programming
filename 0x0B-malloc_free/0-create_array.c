6f21e68fa2d648ec8bdac1a7e017a54465731151
elakhyari <reapergames0159@gmail.com>
2023-07-12 01:22:04 +0100

    Fixing CreatArray

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdiolib.h>
+#include <stdlib.h>
 /**
  * create_array - function that creates an array of chars, and initializes it
  *                with a specific char.
