d2bed31f0cb65e8ab98b253d5f7fec94a1b941e4
elakhyari <reapergames0159@gmail.com>
2023-07-17 15:28:53 +0100

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/*
+ * struct dog - a struct named dog
+ *
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Owner's name
+ *
+ * Description : A structure defining a dog's name age and owner's name
+ */
+
+struct dog
+{
+	float age;
+	char *name;
+	char *owner;
+};
+
+#endif
