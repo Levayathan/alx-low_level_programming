8600ff8ab7fc31f58615c74dd44f35e5697c64ec
levayathan <reapergames0159@gmail.com>
2023-10-21 01:30:49 +0100

    Dynamic libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Levayathan/alx-low_level_programming/blob/main/0x18-dynamic_libraries/libmask.so
+wget -P /tmp https://shorturl.at/ITY14
 export LD_PRELOAD=/tmp/libmask.so
